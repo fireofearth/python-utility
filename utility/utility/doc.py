83d7e13beea0b25307fe6d3b6d9c08d4da8ac00a
Colin Chen <colinc@fastmail.com>
2021-10-18 23:26:20 -0700

    update

--- utility/utility/doc.py
+++ utility/utility/doc.py
@@ -1,10 +1,32 @@
 from . import grouper
 
-def results(*args):
+def cube():
+    return """
+         +--------+
+        /        /|
+       /  top   / |
+      /        /  |
+     /        /   |
+    +--------+    |
+    |        |side|
+    |        |    +
+    | front  |   /
+    |        |  /
+    |        | /
+    |        |/
+    +--------+
+    """
+
+def results(*args, start=None, end=None):
     """Print results in documentation"""
+    if start is not None:
+        print(start)
+        print()
     groups = grouper(args, 2)
     for i, (s, o) in enumerate(grouper(args, 2)):
         print(s)
         print(o)
         if i < len(groups) - 1:
             print()
+    if end is not None:
+        print(end)
